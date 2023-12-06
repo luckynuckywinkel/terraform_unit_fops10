e171d08fc3cf368bbb6ef9ad53e4d454d9d10f78
root <root@terrhost>
2023-12-06 06:35:18 +0000

    Task05 - done, fixed

--- 02/src/locals.tf
+++ 02/src/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  vm_web_name = "${var.vm_web_instance_name}-${var.vm_web_image_family}" #"webserver" #${var.vm_web_instance_name}"
+  vm_web_name = "${var.vm_web_instance_name}-${var.vm_web_image_family}"
   vm_db_name  = "${var.vm_db_instance_name}-${var.vm_db_image_family}"
 }
